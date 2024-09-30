8c4845f77013cc2015493d5b9a6132812ac5a203
Erwan GRAIRE <erwan.graire@student.junia.com>
2024-09-30 12:26:59 +0200

    cook exo 5

--- TD1/TD1_5.cpp
+++ TD1/TD1_5.cpp
@@ -1,12 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int jeudupendu(char mot) {
+int jeudupendu(char mot[]) {
+	cout << "Votre mot a bien ete enregistre que le jeu commence !\n";
+	char lettre;
+	cout << "Saisir une lettre : ";
+	cin >> lettre;
+	for (int i; i < strlen(mot); i++) {
+		if (lettre == mot[i]) {
 
+		}
+	}
+
+
+
+	return 0;
 }
 
 
 
 int main() {
+	char mot[50];
 
+	cout << "Saisissez votre mot a trouver : ";
+	cin >> mot;
+
+
+
+	jeudupendu(mot);
+
+	return 0;
 }
