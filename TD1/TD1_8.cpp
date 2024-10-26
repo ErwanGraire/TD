62f36ad4039beb5e36146fb817c92c7e60bb8872
Erwan GRAIRE <erwan.graire@student.junia.com>
2024-10-26 16:10:02 +0200

    fini td1 et td2 exo1 fini

--- TD1/TD1_8.cpp
+++ TD1/TD1_8.cpp
@@ -1,11 +1,8 @@
-/*Ecrire un programme qui permet d’allouer un tableau à deux dimensions n par m initialisé par des
-valeurs aléatoires qui seront triés dans un ordre croissant colonne par colonne(attention à
-	l’agencement en mémoire des valeurs dans un tableau à deux dimensions)*/
-
 #include <iostream>
 #include <vector>
-#include <cstdlib>  
-#include <ctime> 
+#include <ctime>
+#include <cstdlib>
+
 using namespace std;
 
 int main() {
@@ -17,13 +14,51 @@ int main() {
     int m;
     cin >> m;
 
-    vector< vector<int>> matrix(n, vector<int>(m));
+    vector<vector<int>> matrix(n, vector<int>(m));
     srand(static_cast<unsigned int>(time(0)));
 
+    // Remplir le tableau avec des valeurs aléatoires
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             matrix[i][j] = rand() % 10;
         }
     }
 
+    // Affichage du tableau généré avant tri
+    cout << "Le tableau genere est :" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Tri à bulles directement dans le tableau 2D
+    bool swapped;
+    do {
+        swapped = false;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m - 1; j++) {
+                if (matrix[i][j] > matrix[i][j + 1]) {
+                    swap(matrix[i][j], matrix[i][j + 1]);
+                    swapped = true;
+                }
+            }
+            if (i < n - 1 && matrix[i][m - 1] > matrix[i + 1][0]) {
+                swap(matrix[i][m - 1], matrix[i + 1][0]);
+                swapped = true;
+            }
+        }
+    } while (swapped);
+
+    // Affichage du tableau trié
+    cout << "Le tableau trie est :" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
--- TD2/TD2_1.cpp
+++ TD2/TD2_1.cpp
@@ -1,3 +1,17 @@
-int main() {
+#include <iostream>
+#define multiplier(x,y)((x) * (y))
+#include <cstdlib>  
+using namespace std;
 
+int main(void) {
+	
+	int a;
+	int b;
+	cout << "Entrer une valeur : " << endl;
+	cin >> a;
+	cout << "entrez une atre valeur :" << endl;
+	cin >> b;
+	cout << "Vos valeur sont " << a << " et " << b << endl;
+	cout << multiplier(a + 1, b + 1) << endl;
+	
 }
